d43b1547a1aa0b722a8cefbc40768123d229e164
Annie Davis <anniecdavis27@gmail.com>
2020-07-21 20:47:37 -0400

    updated

--- models/project.js
+++ models/project.js
@@ -3,7 +3,8 @@ const mongoose = require('../db/connection.js');
 const projectsSchema = new mongoose.Schema({
     name: String,
     urlLive: String,
-    urlGit: String
+    urlGit: String,
+    description: String
 });
 
 const Projects = mongoose.model('Projects', projectsSchema);
